0ef29498104687641e457ec6565583091dbc04ad
FuseHelium-3 <supremacyxxxxx@gmail.com>
2020-03-11 16:27:21 +0800

    feat
    update getData.js

---
+++ src/getData.js
@@ -0,0 +1,9 @@
+var dataUrl  = "https://lab.ahusmart.com/nCoV/";
+var dataUrlBackup  = "https://lab.ahusmart.com/nCoV/";
+
+$(document).ready(function () {
+    initCoreData();
+    initTrendChart();
+    initAddChart();
+});
+
